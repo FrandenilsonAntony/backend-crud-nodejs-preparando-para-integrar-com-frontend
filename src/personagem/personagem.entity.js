218df235f42613e7aae7353d8a5e171e0e731522
Frandenilson Antony <iabyantony88@gmail.com>
2024-06-09 17:31:56 +0000

    Campos `Imagem` e `evoluiPara` adcionados.

--- src/personagem/personagem.entity.js
+++ src/personagem/personagem.entity.js
@@ -2,9 +2,19 @@ const Joi = require('joi');
 
 const personagem = Joi.object({
     nome: Joi.string()
-    .min(3)
-    .max(30)
-    .required(),
+     .min(1)
+     .max(30)
+     .required(),
+
+    imagem: Joi.string()
+     .uri()
+     .required(),
+   
+    evoluiPara: Joi.string()
+     .min(1)
+     .max(30)
+     .optional()
+
 });
 
 module.exports = personagem
